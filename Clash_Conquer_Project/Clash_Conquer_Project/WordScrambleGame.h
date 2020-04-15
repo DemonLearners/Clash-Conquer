92911107fa0580a3fa5a07f2707aa79db9cdb3de
Orion-ctrl <51823686+Orion-ctrl@users.noreply.github.com>
2020-04-15 15:12:17 -0500

    Create WordScrambleGame.h
    Word Scramble Game header file

---
+++ Clash_Conquer_Project/Clash_Conquer_Project/WordScrambleGame.h
@@ -0,0 +1,20 @@
+#ifndef WORDSCRAMBLEGAME_H
+#define WORDSCRAMBLEGAME_H
+
+#include <iostream>
+
+using namespace std;
+
+class WordScrambleGame{
+private:
+	int guessCount = 3;
+	string word[3] = {"MICROSCOPE","TEMPERATURE","ELEPHANT"};
+    string nword[3]={"PERCSOICMO","EMEERTRUAPT","ATEPLHEN"};
+	string input;
+	int point;
+	void Initialize();
+public:
+	void Play();
+};
+
+#endif
