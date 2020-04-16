cdaa305944efd4a0d06cf2e52d4d265fbdc6e80d
cielomarte <56612404+cielomarte@users.noreply.github.com>
2020-04-15 23:44:50 -0500

    Create Character.h

---
+++ Clash_Conquer_Project/Clash_Conquer_Project/Character.h
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+chass Character
+{
+    public:
+
+    int defense;
+    bool dead;
+    int attack;
+    int health;
+    int damage;
+    void playerHealth();
+    void playerAttack_();
+    void takeDamage();
+    void Death();
+    void takeMedKit();
+    void displayPlayerStats();
+};
+
+#endif 
