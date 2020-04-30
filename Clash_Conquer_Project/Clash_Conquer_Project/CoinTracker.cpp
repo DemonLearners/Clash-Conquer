15d766079d8dc09ba3ff9a7c0046b0ff20f99e4b
Orion-ctrl <51823686+Orion-ctrl@users.noreply.github.com>
2020-04-30 17:36:52 -0500

    Update CoinTracker.cpp

--- Clash_Conquer_Project/Clash_Conquer_Project/CoinTracker.cpp
+++ Clash_Conquer_Project/Clash_Conquer_Project/CoinTracker.cpp
@@ -1,1 +1,13 @@
 #include "CoinTracker.h"
+
+void CheckUnlockables(Player* pPlayer){
+	if(!pPlayer->coinTracker->equipments.empty()){
+		if(pPlayer->coinTracker->coinAmount >= pPlayer->coinTracker->equipments[0].coinAmount){
+				pPlayer->ChangeEquipment(pPlayer->coinTracker->equipments[0].name,pPlayer->coinTracker->equipments[0].coinAmount,
+			pPlayer->coinTracker->equipments[0].armor);
+			pPlayer->coinTracker->coinAmount-=pPlayer->coinTracker->equipments[0].coinAmount;
+			pPlayer->coinTracker->equipments.erase(pPlayer->coinTracker->equipments.begin());
+		}
+	}
+}
+
